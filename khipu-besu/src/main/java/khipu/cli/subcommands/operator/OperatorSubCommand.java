56c8893be13c64140db47f37a427fe83626814cd
Caoyuan Deng <dcaoyuan@gmail.com>
2020-07-13 15:01:09 -0700

    A runnable minimum set of besu classes.

---
+++ khipu-besu/src/main/java/khipu/cli/subcommands/operator/OperatorSubCommand.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright ConsenSys AG.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+package khipu.cli.subcommands.operator;
+
+import static khipu.cli.subcommands.operator.OperatorSubCommand.COMMAND_NAME;
+
+import java.io.PrintStream;
+import khipu.cli.BesuCommand;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Model.CommandSpec;
+import picocli.CommandLine.ParentCommand;
+import picocli.CommandLine.Spec;
+
+/** Operator related sub-command */
+@Command(
+    name = COMMAND_NAME,
+    description = "Operator related actions such as generating configuration and caches.",
+    mixinStandardHelpOptions = true,
+    subcommands = {GenerateBlockchainConfig.class, GenerateLogBloomCache.class})
+public class OperatorSubCommand implements Runnable {
+
+  public static final String COMMAND_NAME = "operator";
+  public static final String GENERATE_BLOCKCHAIN_CONFIG_SUBCOMMAND_NAME =
+      "generate-blockchain-config";
+
+  @SuppressWarnings("unused")
+  @ParentCommand
+  BesuCommand parentCommand; // Picocli injects reference to parent command
+
+  @SuppressWarnings("unused")
+  @Spec
+  private CommandSpec spec; // Picocli injects reference to command spec
+
+  private final PrintStream out;
+
+  public OperatorSubCommand(final PrintStream out) {
+    this.out = out;
+  }
+
+  @Override
+  public void run() {
+    spec.commandLine().usage(out);
+  }
+}
